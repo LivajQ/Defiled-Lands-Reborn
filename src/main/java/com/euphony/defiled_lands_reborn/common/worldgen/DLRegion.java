80ee785ba5e0f66b09f83cad7d917861e5acc57e
LivajQ <leviackerman.pl@gmail.com>
2026-01-23 23:37:54 +0100

    Arcane gen

--- src/main/java/com/euphony/defiled_lands_reborn/common/worldgen/DLRegion.java
+++ src/main/java/com/euphony/defiled_lands_reborn/common/worldgen/DLRegion.java
@@ -8,8 +8,11 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.biome.Climate;
+import net.minecraftforge.fml.ModList;
+import terrablender.api.ParameterUtils;
 import terrablender.api.Region;
 import terrablender.api.RegionType;
+import terrablender.api.VanillaParameterOverlayBuilder;
 
 import java.util.function.Consumer;
 
@@ -54,15 +57,35 @@ public class DLRegion extends Region {
                 .build().forEach(point -> builder.add(point, DLBiomes.DEFILED_HILLS));
 
         builder.build().forEach(mapper);
-
+       
          */
-        this.addModifiedVanillaOverworldBiomes(mapper, builder -> {
-            this.addBiomeSimilar(mapper, Biomes.PLAINS, DLBiomes.DEFILED_PLAINS);
-            this.addBiomeSimilar(mapper, Biomes.DESERT, DLBiomes.DEFILED_DESERT);
-            this.addBiomeSimilar(mapper, Biomes.WINDSWEPT_HILLS, DLBiomes.DEFILED_HILLS);
-            this.addBiomeSimilar(mapper, Biomes.SNOWY_PLAINS, DLBiomes.DEFILED_SNOWY_PLAINS);
-            this.addBiomeSimilar(mapper, Biomes.BIRCH_FOREST, DLBiomes.TENEBRA_FOREST);
-            this.addBiomeSimilar(mapper, Biomes.DARK_FOREST, DLBiomes.VILESPINE_FOREST);
-        });
+        
+        if (ModList.get().isLoaded("arcanetweaks")) {
+            VanillaParameterOverlayBuilder builder = new VanillaParameterOverlayBuilder();
+            
+            ParameterUtils.ParameterPointListBuilder stupidBig = new ParameterUtils.ParameterPointListBuilder()
+                    .temperature(Climate.Parameter.span(-1.0F, 1.0F))
+                    .humidity(Climate.Parameter.span(-1.0F, 1.0F))
+                    .continentalness(Climate.Parameter.span(-1.0F, 1.0F))
+                    .erosion(Climate.Parameter.span(-1.0F, 1.0F))
+                    .depth(Climate.Parameter.span(-1.0F, 1.0F))
+                    .weirdness(Climate.Parameter.span(-1.0F, 1.0F));
+            
+            stupidBig.build().forEach(p ->
+                    builder.add(p, DLBiomes.DEFILED_PLAINS)
+            );
+            
+            builder.build().forEach(mapper);
+        }
+        else {
+            this.addModifiedVanillaOverworldBiomes(mapper, builder -> {
+                this.addBiomeSimilar(mapper, Biomes.PLAINS, DLBiomes.DEFILED_PLAINS);
+                this.addBiomeSimilar(mapper, Biomes.DESERT, DLBiomes.DEFILED_DESERT);
+                this.addBiomeSimilar(mapper, Biomes.WINDSWEPT_HILLS, DLBiomes.DEFILED_HILLS);
+                this.addBiomeSimilar(mapper, Biomes.SNOWY_PLAINS, DLBiomes.DEFILED_SNOWY_PLAINS);
+                this.addBiomeSimilar(mapper, Biomes.BIRCH_FOREST, DLBiomes.TENEBRA_FOREST);
+                this.addBiomeSimilar(mapper, Biomes.DARK_FOREST, DLBiomes.VILESPINE_FOREST);
+            });
+        }
     }
 }
